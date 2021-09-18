60bb637a3a6b4b2d72567871672f67800eb21cf3
messier109 <asimonov05@gmail.com>
2021-09-18 10:53:46 +0300

    Минимальный echo bot
    Добавлен ngrok, requirements. Создан минимальный бот.

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 Для старта сервера:
 
-1. docker build -t mus-tgbot .
-2. docker run -it -p 5000:5000 mus-tgbot
+1. Поднять ngrok на 5000 порту
+2. Изменить переменную url на актуальную в app.py/start_bot
+3. docker build -t mus-tgbot .
+4. docker run -it -p 5000:5000 mus-tgbot
+5. Перейти на http://localhost:5000/start_bot
--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, world!'
+@app.route("/", methods=["GET", "POST"])
+def receive_update():
+    if request.method == "POST":
+        print(request.json)
+    return {"ok": True}
+
+@app.route('/start_bot')
+def start_bot():
+    token = '2024499408:AAHWeU_4iRWurzzUMDLETBWmQ6h_amfY9Mg'
+    url = "https://8c2b-109-252-124-11.ngrok.io"
+    requests.post(f'https://api.telegram.org/bot{token}/setWebhook', data={"url": f"{url}"})
+    return 'ok'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
